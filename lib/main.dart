89542d6c4b3843325fbf269c5a7ff72523c3dd6e
Caio <caio.balassoni1510@gmail.com>
2026-09-16 10:37:26 -0300

    Nilsinho widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
+  
+  runApp(const NilsinhoWidget());
 }
 
 class MainApp extends StatelessWidget {
@@ -11,6 +13,26 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
+
+class NilsinhoWidget extends StatefulWidget {
+  const new({super.key});
+
+  @override
+  State<NilsinhoWidget> createState() => _NilsinhoWidgetState();
+}
+
+class _NilsinhoWidgetState extends State<NilsinhoWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Olá Mundo!")
+          )
+        ),
     );
   }
 }
